bd6cdecb95addb2864f6d8e5dedca0a1b7576145
Heiko Huvermann <wp7dev@huvermann.com>
2017-09-19 12:07:31 +0200

    Cleanup: Removed unneccessary usings

--- Colaautomat/Core/Models/WarenausgabeSimulator.cs
+++ Colaautomat/Core/Models/WarenausgabeSimulator.cs
@@ -2,11 +2,7 @@
 using Prism.Events;
 using Prism.Mvvm;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Colaautomat.Core.Models
 {
